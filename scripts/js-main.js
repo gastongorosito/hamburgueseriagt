a0cf29f10cc6d92f5327888f94519d36d7317483
Gastón Gorosito <gorosito.gaston@gmail.com>
2022-12-12 20:38:16 -0300

    arrays

--- scripts/js-main.js
+++ scripts/js-main.js
@@ -23,15 +23,38 @@ let Hamburguesa = prompt(`¿Que Hamburguesa te gustaria comprar?:
 
 switch (Hamburguesa) {
   case "1":
-    alert("Elegiste Hamburguesa Bido, debes abonar $2.500");
+    alert("Elegiste Hamburguesa Bido, debes abonar $2500");
     break;
   case "2":
-    alert("Elegiste Hamburguesa FDX, debes abonar $1.169 .");
+    alert("Elegiste Hamburguesa FDX, debes abonar $1170 .");
     break;
   case "3":
-    alert("Elegiste Hamburguesa El Loco Luca, debes abonar $2.222.");
+    alert("Elegiste Hamburguesa El Loco Luca, debes abonar $2200.");
     break;
   default:
     alert("No elegiste ninguna de las opciones anteriores.");
     break;
 }
+
+//Turnos para pedido
+for (let i = 1; i <= 5; i++) {
+  let ingresarNombre = prompt("Ingresar nombre");
+  alert(" Turno  N° "+i+" Nombre: "+ingresarNombre);}
+
+//precio pagando con tarjeta
+class Producto {
+  constructor(nombre, precio) {
+      this.nombre  = nombre.toUpperCase();
+      this.precio  = precio;
+      this.sumaTarjeta = function () {
+        console.log((this.precio = this.precio * 1.15))
+      }
+  }
+}
+const productos = [];
+productos.push(new Producto("Hamburguesa Bido", "2500"));
+productos.push(new Producto("Hamburguesa FDX", "1170"));
+productos.push(new Producto("Hamburguesa El Loco Luca", "2200"));
+
+for (const producto of productos)
+  producto.sumaTarjeta();
