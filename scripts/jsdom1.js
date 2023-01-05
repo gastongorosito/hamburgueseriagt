bd766572b846b65ac0935f7864da15b4c49e9c8a
Gastón Gorosito <gorosito.gaston@gmail.com>
2023-01-05 17:41:48 -0300

    js

--- scripts/jsdom1.js
+++ scripts/jsdom1.js
@@ -1,7 +1,44 @@
-let elemento;
-
-elemento = document 
-elemento = document.head 
-
-
-console.log(elemento)
+let productos = [
+    {
+      categoria: "Hamburguesa",
+      foto: "loco",
+      nombre: "loco luca",
+      precio: 1850,
+      producto: 1,
+    },
+    {
+      categoria: "Hamburguesa",
+      foto: "bido",
+      nombre: "gordo santi",
+      precio: 1500,
+      producto: 2,
+    },
+    {
+      categoria: "Hamburguesa",
+      foto: "fdx",
+      nombre: "fdx",
+      precio: 2500,
+      producto: 3,
+    },
+  ];
+  
+  let listaProductos = document.querySelector(".contenedor-productos");
+  
+  for (let a of productos) {
+    let contenedor = document.createElement("div");
+    contenedor.innerHTML = `
+  <div class=" contenedor-producto col-lg-12">
+    <span class="badge bg-primary mt-2 mb-4">${a.categoria}</span>
+  <div class="d-flex justify-content-center"><img src="imagenes/${a.foto}.png" alt="" class="img-fluid"></img></div>
+  <div class="contenedor-producto card-body">
+    <h5 class="card-title mt-3">${a.nombre}</h5>
+    <h6 class="mb-3">${a.precio}</h6>
+    <button type="button" class="btn btn-primary boton-compra${a.producto}">COMPRAR</button>
+  </div>
+    </div>`;
+  
+    listaProductos.appendChild(contenedor);
+  }
+  
+ 
+  
